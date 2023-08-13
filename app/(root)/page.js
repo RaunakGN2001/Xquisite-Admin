7395e7e189b8a178c3b7c810faa2f0fa5c8ed5ff
Raunak Gayen <rnk.express@gmail.com>
2023-08-13 16:06:15 +0530

    added zustand for global state management

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,10 +1,18 @@
+'use client';
+
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetupPage() {
     return (
-        <div>
-            <UserButton afterSignOutUrl="/"/>
+        <div className = 'p-4'>
+            <Modal title={'Test'} description={'Test'} isOpen onClose={() => {
+                isOpen = !isOpen;
+            }}>
+                Children
+            </Modal>
         </div>
-    )
+    )   
   }
   
